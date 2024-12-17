fff0874888ad96c6069ccc2c4d9e7d1bae89e925
seop <useop0821@gmail.com>
2024-12-18 02:08:55 +0900

    test

--- src/test/java/com/seoplog/controller/post/PostControllerTest.java
+++ src/test/java/com/seoplog/controller/post/PostControllerTest.java
@@ -1,7 +1,9 @@
 package com.seoplog.controller.post;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.seoplog.domain.post.request.PostCreateRequest;
+import com.seoplog.domain.post.Post;
+import com.seoplog.domain.post.request.PostCreate;
+import com.seoplog.domain.post.request.PostUpdate;
 import com.seoplog.repository.PostRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,11 +13,15 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -38,7 +44,7 @@ class PostControllerTest {
     @Test
     void test() throws Exception {
 
-        PostCreateRequest request = PostCreateRequest.builder()
+        PostCreate request = PostCreate.builder()
                 .title("제목입니다.")
                 .content("내용입니다.")
                 .build();
@@ -57,7 +63,7 @@ class PostControllerTest {
     @Test
     @DisplayName("제목이 없으면 실패한다.")
     void titleNullTest() throws Exception {
-        PostCreateRequest request = PostCreateRequest.builder()
+        PostCreate request = PostCreate.builder()
                 .content("내용입니다.")
                 .build();
 
@@ -77,7 +83,7 @@ class PostControllerTest {
     @DisplayName("/posts 요청시 DB에 값이 저장된다.")
     void savedDB() throws Exception {
         // when
-        PostCreateRequest request = PostCreateRequest.builder()
+        PostCreate request = PostCreate.builder()
                 .title("제목입니다.")
                 .content("내용입니다.")
                 .build();
@@ -92,6 +98,123 @@ class PostControllerTest {
                 .andDo(print());
 
         // then
-        assertEquals(1L,postRepository.count());
+        assertThat(postRepository.count()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("제목은 20글자 이하로 작성해야한다.")
+    void validateTitleLength() throws Exception {
+        PostCreate request = PostCreate.builder()
+                .title("1231552346246212312321451352346")
+                .content("내용입니다.")
+                .build();
+
+        String json = objectMapper.writeValueAsString(request);
+
+        mockMvc.perform(post("/posts")
+                        .contentType(APPLICATION_JSON)
+                        .content(json)
+                )
+                .andExpect(status().isBadRequest())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("글 여러 개 조회")
+    void findAll() throws Exception {
+        Post post1 = Post.builder()
+                .title("123")
+                .content("내용입니다.")
+                .build();
+
+        Post post2 = Post.builder()
+                .title("123")
+                .content("내용입니다.")
+                .build();
+
+        postRepository.saveAll(List.of(post1, post2));
+
+        mockMvc.perform(get("/posts")
+                        .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("페이지를 0으로 요청하면 첫 페이지를 가져온다.")
+    void pageable() throws Exception {
+        List<Post> requestPosts = IntStream.range(0, 20)
+                .mapToObj(i -> Post.builder()
+                        .title("제목 " + i)
+                        .content("내용 " + i)
+                        .build())
+                .toList();
+
+        postRepository.saveAll(requestPosts);
+
+        mockMvc.perform(get("/posts?page=0&size=10")
+                        .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+    @Test
+    @DisplayName("글 수정")
+    void updatePost() throws Exception {
+        Post post = Post.builder()
+                .title("제목")
+                .content("내용")
+                .build();
+        postRepository.save(post);
+
+        PostUpdate postUpdate = PostUpdate.builder()
+                .title("제목2")
+                .content("내용2")
+                .build();
+
+        mockMvc.perform(patch("/posts/{postId}", post.getId())
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(postUpdate)))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("글 삭제")
+    void deletePost() throws Exception {
+        Post post = Post.builder()
+                .title("제목")
+                .content("내용")
+                .build();
+        postRepository.save(post);
+
+        mockMvc.perform(delete("/posts/{postId}", post.getId())
+                        .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 글 조회")
+    void findPost_Exception() throws Exception {
+
+        mockMvc.perform(get("/posts/{postId}", 1L)
+                        .contentType(APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 글 수정")
+    void updatePost_Exception() throws Exception {
+        PostUpdate postUpdate = PostUpdate.builder()
+                .title("제목2")
+                .content("내용2")
+                .build();
+
+        mockMvc.perform(patch("/posts/{postId}", 1L)
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(postUpdate)))
+                .andExpect(status().isNotFound())
+                .andDo(print());
     }
 }
--- src/test/java/com/seoplog/service/PostServiceTest.java
+++ src/test/java/com/seoplog/service/PostServiceTest.java
@@ -1,19 +1,28 @@
 package com.seoplog.service;
 
 import com.seoplog.domain.post.Post;
-import com.seoplog.domain.post.request.PostCreateRequest;
+import com.seoplog.domain.post.request.PostCreate;
+import com.seoplog.domain.post.request.PostSearch;
+import com.seoplog.domain.post.request.PostUpdate;
+import com.seoplog.domain.post.response.PostResponse;
+import com.seoplog.exception.PostNotFound;
 import com.seoplog.repository.PostRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 @Transactional
+@ActiveProfiles("test")
 class PostServiceTest {
 
     @Autowired
@@ -26,7 +35,7 @@ class PostServiceTest {
     @DisplayName("글 작성")
     void test() throws Exception {
         //given
-        PostCreateRequest request = PostCreateRequest.builder()
+        PostCreate request = PostCreate.builder()
                 .title("제목")
                 .content("내용")
                 .build();
@@ -35,10 +44,10 @@ class PostServiceTest {
         postService.write(request);
 
         //then
-        assertEquals(1L, postRepository.count());
+        assertThat(postRepository.count()).isEqualTo(1);
         Post post = postRepository.findAll().get(0);
-        assertEquals("제목", post.getTitle());
-        assertEquals("내용", post.getContent());
+        assertThat(post.getTitle()).isEqualTo("제목");
+        assertThat(post.getContent()).isEqualTo("내용");
     }
 
     @Test
@@ -52,9 +61,87 @@ class PostServiceTest {
         Post savedPost = postRepository.save(post);
 
         //when
-        postService.findById(savedPost.getId());
+        PostResponse response = postService.findPost(savedPost.getId());
 
         //then
-        assertNotNull(post);
+        assertThat(post).isNotNull();
+        assertThat(response.getTitle()).isEqualTo("제목");
+        assertThat(response.getContent()).isEqualTo("내용");
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 글을 조회하면 예외가 발생한다.")
+    void findPost_Exception() throws Exception {
+        //given
+        Long postId = 1L;
+
+        //when & then
+        assertThatThrownBy(() -> postService.findPost(postId))
+                .isInstanceOf(PostNotFound.class);
+    }
+
+    @Test
+    @DisplayName("글을 id값으로 내림차순해서 페이지를 조회한다.")
+    void postPageableDesc() throws Exception {
+        //given
+        List<Post> requestPosts = IntStream.range(0, 20)
+                .mapToObj(i -> Post.builder()
+                        .title("제목 " + i)
+                        .content("내용 " + i)
+                        .build())
+                .toList();
+
+        postRepository.saveAll(requestPosts);
+
+        PostSearch postSearch = PostSearch.builder()
+                .page(1)
+                .size(10)
+                .build();
+
+        //when
+        List<PostResponse> posts = postService.findAll(postSearch);
+
+        //then
+        assertThat(posts).hasSize(10);
+        assertThat(posts.get(0).getTitle()).isEqualTo("제목 19");
+    }
+
+    @Test
+    @DisplayName("글 수정")
+    void updatePost() throws Exception {
+        //given
+        Post post = Post.builder()
+                .title("제목")
+                .content("내용")
+                .build();
+        postRepository.save(post);
+
+        PostUpdate update = PostUpdate.builder()
+                .title(null)
+                .content("내용2")
+                .build();
+        //when
+        postService.update(post.getId(), update);
+
+        //then
+        assertThat(post.getTitle()).isEqualTo("제목");
+        assertThat(post.getContent()).isEqualTo("내용2");
+    }
+
+    @Test
+    @DisplayName("글 삭제")
+    void deletePost() throws Exception {
+        //given
+        Post post = Post.builder()
+                .title("제목")
+                .content("내용")
+                .build();
+        postRepository.save(post);
+
+        //when
+        postService.deletePost(post.getId());
+
+        //then
+        assertThat(postRepository.count()).isZero();
     }
 }
