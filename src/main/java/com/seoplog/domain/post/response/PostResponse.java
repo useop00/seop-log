6200755a0d64d8456f768990cc60cacc8501e19d
seop <useop0821@gmail.com>
2024-12-18 02:10:08 +0900

    생성시간 추가

--- src/main/java/com/seoplog/domain/post/response/PostResponse.java
+++ src/main/java/com/seoplog/domain/post/response/PostResponse.java
@@ -3,20 +3,22 @@ package com.seoplog.domain.post.response;
 import com.seoplog.domain.post.Post;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
-@Setter
 public class PostResponse {
-    private Long id;
-    private String title;
-    private String content;
+    private final Long id;
+    private final String title;
+    private final String content;
+    private final LocalDateTime createDateTime;
 
     @Builder
-    private PostResponse(Long id, String title, String content) {
+    private PostResponse(Long id, String title, String content, LocalDateTime createDateTime) {
         this.id = id;
         this.title = title;
         this.content = content;
+        this.createDateTime = createDateTime;
     }
 
     public static PostResponse of(Post post) {
@@ -24,6 +26,7 @@ public class PostResponse {
                 .id(post.getId())
                 .title(post.getTitle())
                 .content(post.getContent())
+                .createDateTime(post.getCreatedDateTime())
                 .build();
     }
 }
