8870a3d3f9221fedc697fbb4f97c5510ef030903
seop <useop0821@gmail.com>
2024-12-18 02:06:40 +0900

    querydsl 페이징

--- src/main/java/com/seoplog/repository/PostRepository.java
+++ src/main/java/com/seoplog/repository/PostRepository.java
@@ -3,5 +3,5 @@ package com.seoplog.repository;
 import com.seoplog.domain.post.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
 }
