720deb27267aff9022bcd76ff412f69b8c6d6b2d
seop <useop0821@gmail.com>
2024-12-18 02:11:21 +0900

    수정,삭제 메서드 +

--- src/main/java/com/seoplog/controller/post/PostController.java
+++ src/main/java/com/seoplog/controller/post/PostController.java
@@ -1,13 +1,17 @@
 package com.seoplog.controller.post;
 
 import com.seoplog.controller.ApiResponse;
-import com.seoplog.domain.post.request.PostCreateRequest;
+import com.seoplog.domain.post.request.PostCreate;
+import com.seoplog.domain.post.request.PostSearch;
+import com.seoplog.domain.post.request.PostUpdate;
 import com.seoplog.domain.post.response.PostResponse;
 import com.seoplog.service.PostService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -17,13 +21,29 @@ public class PostController {
     private final PostService postService;
 
     @PostMapping("/posts")
-    public ApiResponse<PostResponse> post(@RequestBody @Valid PostCreateRequest request) {
+    public ApiResponse<PostResponse> post(@RequestBody @Valid PostCreate request) {
 
         return ApiResponse.ok(postService.write(request));
     }
 
     @GetMapping("/posts/{postId}")
     public ApiResponse<PostResponse> findPost(@PathVariable Long postId) {
-        return ApiResponse.ok(postService.findById(postId));
+        return ApiResponse.ok(postService.findPost(postId));
+    }
+
+    @GetMapping("/posts")
+    public ApiResponse<List<PostResponse>> findAll(@ModelAttribute PostSearch postSearch){
+        return ApiResponse.ok(postService.findAll(postSearch));
+    }
+
+    @PatchMapping("/posts/{postId}")
+    public ApiResponse<PostResponse> updatePost(@PathVariable Long postId, @RequestBody @Valid PostUpdate request) {
+        return ApiResponse.ok(postService.update(postId, request));
+    }
+
+    @DeleteMapping("/posts/{postId}")
+    public ApiResponse<Void> deletePost(@PathVariable Long postId) {
+        postService.deletePost(postId);
+        return ApiResponse.ok();
     }
 }
