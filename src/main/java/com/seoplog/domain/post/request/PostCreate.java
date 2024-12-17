e20b0f5431a77602a2bd824d41b10be6a8b70537
seop <useop0821@gmail.com>
2024-12-18 02:10:58 +0900

    PostCreateRequest

--- src/main/java/com/seoplog/domain/post/request/PostCreate.java
+++ src/main/java/com/seoplog/domain/post/request/PostCreate.java
@@ -2,24 +2,24 @@ package com.seoplog.domain.post.request;
 
 import com.seoplog.domain.post.Post;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
-@ToString
 @Setter
 @Getter
-public class PostCreateRequest {
+public class PostCreate {
 
     @NotBlank(message = "제목을 입력해주세요.")
+    @Size(max = 20, message = "제목은 20글자 이하로 입력해주세요.")
     private String title;
 
     @NotBlank(message = "내용을 입력해주세요.")
     private String content;
 
     @Builder
-    private PostCreateRequest(String title, String content) {
+    private PostCreate(String title, String content) {
         this.title = title;
         this.content = content;
     }
