49453ac173e9ef6cc145ffb48c22991858119e49
seop <useop0821@gmail.com>
2024-12-28 01:25:01 +0900

    feat : user 추가

--- src/main/java/com/seoplog/service/EntityFinder.java
+++ src/main/java/com/seoplog/service/EntityFinder.java
@@ -1,8 +1,11 @@
 package com.seoplog.service;
 
 import com.seoplog.domain.post.Post;
+import com.seoplog.domain.user.User;
 import com.seoplog.exception.PostNotFound;
+import com.seoplog.exception.UserNotFound;
 import com.seoplog.repository.post.PostRepository;
+import com.seoplog.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -11,9 +14,15 @@ import org.springframework.stereotype.Component;
 public class EntityFinder {
 
     private final PostRepository postRepository;
+    private final UserRepository userRepository;
 
     public Post getPostId(Long postId) {
         return postRepository.findById(postId)
                 .orElseThrow(PostNotFound::new);
+    }
+
+    public User getLoginUser(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(UserNotFound::new);
     }
 }
