701c7620c7a54c1687609f0bcb079e494866116a
seop <useop0821@gmail.com>
2024-12-28 00:56:34 +0900

    feat : Add login Request

--- src/main/java/com/seoplog/domain/user/request/Login.java
+++ src/main/java/com/seoplog/domain/user/request/Login.java
@@ -1,6 +1,5 @@
 package com.seoplog.domain.user.request;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,15 +8,12 @@ import lombok.Setter;
 @Setter
 public class Login {
 
-    @NotBlank(message = "아이디를 입력해주세요.")
-    private String account;
-
-    @NotBlank(message = "비밀번호를 입력해주세요.")
+    private String username;
     private String password;
 
     @Builder
-    private Login(String account, String password) {
-        this.account = account;
+    private Login(String username, String password) {
+        this.username = username;
         this.password = password;
     }
 }
